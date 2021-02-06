086e3531e07ef31bfed14f0703217af474c2ab37
Dustin Milotte <dmilotte@gmail.com>
2021-02-06 16:30:59 -0500

    ♻️ Clean up folder structure, 🚧 begin Rocket reset function

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // This is where we will list the public and private variables for the Rocket class
+    
+    private Vector3 startingPosition;
+    private Quaternion startingRotation;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // store the rocket's starting position and rotation so we can reset it if necessary
+        startingPosition = transform.position;
+        startingRotation = transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void ResetRocket()
+    {
+      
+    }
+}
