e55e38928aa9063da27861821064b97149f2d80d
Dustin Milotte <dmilotte@gmail.com>
2021-06-06 10:04:48 -0400

    ✨ Add RockObstacle prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -41,6 +41,7 @@ public class GameManager : MonoBehaviour
     {
         Invoke("ResetLevel", levelLoadDelay);
     }
+    
     public void OnGoalReached()
     {
         Invoke("LoadNextLevel", levelLoadDelay);
--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditorInternal;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
@@ -74,6 +70,8 @@ public class Rocket : MonoBehaviour
         successParticles.Play();
         audioSource.PlayOneShot(successSound);
         sequenceHasStarted = true;
+        // call the method on GameManager
+        
     }
 
     private void StartCrashSequence()
